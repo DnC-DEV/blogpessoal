f7f5e3edf795f50ce7741df1d50d5afcd2feb76a
Daniel Correa <daniel.correa1907@hotmail.com>
2022-10-28 11:03:20 -0300

    Atualização do arquivo main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact('Generation Brasil', 'https://brazil.generation.org', 'generation@gmail.com')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-03:00';
 
