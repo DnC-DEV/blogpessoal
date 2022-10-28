68c763984651261bf7354c4bb7b14e1c9c09968d
Daniel Correa <daniel.correa1907@hotmail.com>
2022-10-28 11:04:55 -0300

    Atualização do arquivo postagem.entity

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -8,28 +8,35 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger/dist/decorators';
 
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @IsNotEmpty()
   @Column({ length: 100, nullable: false })
+  @ApiProperty()
   titulo: string;
 
   @IsNotEmpty()
   @Column({ length: 100, nullable: false })
+  @ApiProperty()
   texto: string;
 
   @UpdateDateColumn()
+  @ApiProperty()
   data: Date;
-
+  
+  @ApiProperty({ type: () => Tema})
   @ManyToOne(() => Tema, (tema) => tema.postagem, {
     onDelete: 'CASCADE',
   })
   tema: Tema;
 
+  @ApiProperty({ type: () => Postagem})
   @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
     onDelete: 'CASCADE',
   })
