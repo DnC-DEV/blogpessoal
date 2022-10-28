06eb55191b0ce522bbc5374a4b73725ed04248da
Daniel Correa <daniel.correa1907@hotmail.com>
2022-10-28 11:05:35 -0300

    Atualização do arquivo tema.entity

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,16 +1,20 @@
 import { IsNotEmpty } from 'class-validator';
 import { Postagem } from '../../postagem/entities/postagem.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger/dist/decorators';
 
 @Entity({ name: 'tb_temas' })
 export class Tema {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty()
   descricao: string;
 
+  @ApiProperty()
   @OneToMany(() => Postagem, (postagem) => postagem.tema)
   postagem: Postagem[];
 }
