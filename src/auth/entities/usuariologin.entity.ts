0df1b0eb82cb163477f8bbcf65cc2deb1d569b72
Daniel Correa <daniel.correa1907@hotmail.com>
2022-10-28 11:04:12 -0300

    Atualização do arquivo usuariologin.entity

--- src/auth/entities/usuariologin.entity.ts
+++ src/auth/entities/usuariologin.entity.ts
@@ -1,5 +1,9 @@
-export class UsuarioLogin {
-  public usuario: string;
+import { ApiProperty } from "@nestjs/swagger/dist/decorators";
 
+export class UsuarioLogin {
+  @ApiProperty()
+  public usuario: string;
+  
+  @ApiProperty()
   public senha: string;
 }
