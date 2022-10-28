b14387b757d7b1b3b639460fe1e14cda7fea952b
Daniel Correa <daniel.correa1907@hotmail.com>
2022-10-28 11:03:43 -0300

    Atualização do arquivo auth.controller

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, HttpStatus, HttpCode, Post, Body } from "@nestjs/common";
 import { UseGuards } from "@nestjs/common/decorators/core/use-guards.decorator";
+import { ApiTags } from "@nestjs/swagger/dist/decorators";
 import { UsuarioLogin } from "../entities/usuariologin.entity";
 import { LocalAuthGuard } from "../guard/local.auth.guard";
 import { AuthService } from "../services/auth.service";
 
+@ApiTags('Usuario')
 @Controller('/auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
